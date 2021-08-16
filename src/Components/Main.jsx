9681faf3b66fb3da0a0bf96b16d08b08238407c9
jallend1 <jallend1@gmail.com>
2021-08-16 16:29:44 -0700

    Adds image and updates primary color to match image

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import logo from '../assets/images/twodogs-vertical.png';
 
 const Main = () => {
   return (
     <div className='page main'>
       <div className='left'>
-        <h2>Two Dogs Developers</h2>
+        <h2>Two Dogs Web Development</h2>
         <h1>
           Focused on creating solutions that make a difference for
-          <span className='emphasis'> your business</span>.
+          <span className='emphasis'> your</span> business.
         </h1>
         {/* <button>Get in touch.</button> */}
       </div>
-      <div className='right'>Logo here</div>
+      <div className='right'>
+        <div className='logo'>
+          <img src={logo} alt='Two Dogs' />
+        </div>
+      </div>
     </div>
   );
 };
